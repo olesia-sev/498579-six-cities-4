d7611c2d1217ae33f60fcbba16fa82a33283da14
olesia-sev <olesya0191@gmail.com>
2020-06-13 23:04:01 +0300

    module2-task1: Добавляет PropTypes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import * as PropTypes from 'prop-types';
 import {Main} from '../main/main';
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {optionsAmount} = props;
+  const {optionsAmount, cardTitles} = props;
 
   return <Main
     optionsAmount = {optionsAmount}
+    cardTitles = {cardTitles}
   />;
+};
+
+App.propTypes = {
+  optionsAmount: PropTypes.number.isRequired,
+  cardTitles: PropTypes.arrayOf(
+      PropTypes.string
+  ).isRequired,
 };
 
 export {App};
--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import {App} from "./components/app/app.jsx";
 const init = () => {
   const settings = {
     optionsAmount: 500,
+    placeCardTitles: [
+      `Beautiful & luxurious apartment at great location`,
+      `Wood and stone place`,
+      `Nice, cozy, warm big bed apartment`
+    ],
   };
 
   ReactDOM.render(
       <App
         optionsAmount = {settings.optionsAmount}
+        cardTitles = {settings.placeCardTitles}
       />,
       document.querySelector(`#root`)
   );
