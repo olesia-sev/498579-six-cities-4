a2e84d2c27796ae77374187f54cff3c015ee51ee
olesia-sev <olesya0191@gmail.com>
2020-06-22 15:58:03 +0300

    module3-task1: Удаляет функцию

--- src/components/places-list/places-list.jsx
+++ src/components/places-list/places-list.jsx
@@ -5,15 +5,11 @@ import {PlaceCard} from '../place-card/place-card';
 const PlacesList = ({offers}) => {
   const [, setActiveOffer] = useState(null);
 
-  const updateActiveOffer = (offer) => {
-    setActiveOffer(offer);
-  };
-
   return (
     <div className="cities__places-list places__list tabs__content">
       {
         offers.map((offer) => (
-          <PlaceCard key={offer.id} offer={offer} setActiveOffer={updateActiveOffer} />
+          <PlaceCard key={offer.id} offer={offer} setActiveOffer={setActiveOffer} />
         ))
       }
     </div>
